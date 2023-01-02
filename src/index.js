514b03938bfe56bfb064ad7ab043504da02e4fe8
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-02 15:19:10 +0530

    wrapped context providers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { LoadingProvider } from "./Contexts/LoadingContext";
+import { UserProvider } from "./Contexts/UserContext";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SnackbarProvider maxSnack={3}>
+        <LoadingProvider>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </LoadingProvider>
+      </SnackbarProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
