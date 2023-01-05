897c0aa79d8bbe13fc746ee95519dad87b6df9c0
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-05 21:02:56 +0530

    container for content after nav

---
+++ src/Components/BoxContainer.js
@@ -0,0 +1,19 @@
+import { Box, height } from "@mui/system";
+import React from "react";
+
+export default function BoxContainer({ children }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        minWidth: 300,
+        width: "100%",
+        height: "calc(100vh - 68.5px)",
+        overflow: "auto",
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
