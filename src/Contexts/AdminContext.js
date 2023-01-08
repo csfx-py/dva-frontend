c3e6cdc0a92c21cfe320d53ea059186907130e71
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-08 15:23:04 +0530

    crud depts

--- src/Contexts/AdminContext.js
+++ src/Contexts/AdminContext.js
@@ -22,6 +22,22 @@ export const AdminProvider = ({ children }) => {
     }
   };
 
+  const getDepts = async () => {
+    try {
+      setLoading(true);
+
+      const res = await API.get("/admin/depts");
+
+      if (!res.data?.success) throw new Error(res.data.message);
+
+      return { success: true, depts: res.data.depts };
+    } catch (error) {
+      return { success: false, error: error.response?.data || error };
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const userApproval = async (userId, accessLevel) => {
     try {
       setLoading(true);
@@ -42,11 +58,75 @@ export const AdminProvider = ({ children }) => {
     }
   };
 
+  const addDept = async (name, code) => {
+    try {
+      setLoading(true);
+      const res = await API.post("/admin/create-dept", {
+        name,
+        code,
+      });
+
+      if (!res.data?.success) throw new Error(res.data.message);
+
+      const { depts } = await getDepts();
+
+      return { success: true, depts };
+    } catch (error) {
+      return { success: false, error: error.response?.data || error };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateDept = async (deptId, name, code) => {
+    try {
+      setLoading(true);
+      const res = await API.put("/admin/edit-dept", {
+        deptId,
+        name,
+        code,
+      });
+
+      if (!res.data?.success) throw new Error(res.data.message);
+
+      const { depts } = await getDepts();
+
+      return { success: true, depts };
+    } catch (error) {
+      return { success: false, error: error.response?.data || error };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteDept = async (deptId) => {
+    try {
+      setLoading(true);
+      const res = await API.delete("/admin/delete-dept", {
+        data: { deptId },
+      });
+
+      if (!res.data?.success) throw new Error(res.data.message);
+
+      const { depts } = await getDepts();
+
+      return { success: true, depts };
+    } catch (error) {
+      return { success: false, error: error.response?.data || error };
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <AdminContext.Provider
       value={{
         userApproval,
         getAllUsers,
+        getDepts,
+        addDept,
+        updateDept,
+        deleteDept,
       }}
     >
       {children}
