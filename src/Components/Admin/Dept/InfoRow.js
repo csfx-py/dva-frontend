70d57967794e9a8eccf284eb7a3d5b43f0438c8d
Shreyas Prabhu <csfxshreyas@gmail.com>
2023-01-08 15:22:39 +0530

    rows for dept table

---
+++ src/Components/Admin/Dept/InfoRow.js
@@ -0,0 +1,66 @@
+import { DeleteForever, Edit } from "@mui/icons-material";
+import { Button, TableCell, TableRow } from "@mui/material";
+import { motion } from "framer-motion";
+
+export default function InfoRow({
+  setRows,
+  columns,
+  row,
+  index,
+  handleDelete,
+}) {
+  return (
+    <TableRow
+      hover
+      component={motion.tr}
+      initial={{ y: -200 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.5 }}
+      exit={{ x: 200 }}
+    >
+      {columns.map((column) => {
+        const value = row[column.id];
+        return (
+          <TableCell key={column.id} align={column.align}>
+            {column.format && typeof value === "number"
+              ? column.format(value)
+              : value}
+          </TableCell>
+        );
+      })}
+
+      <TableCell
+        align="center"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 1,
+        }}
+      >
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={() =>
+            setRows((prev) => {
+              const _ = [...prev];
+              _[index].edit = true;
+              return _;
+            })
+          }
+        >
+          <Edit /> Edit
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          fullWidth
+          onClick={() => handleDelete(row._id)}
+        >
+          <DeleteForever />
+          Delete
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
